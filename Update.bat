951c2de25619c2bc963fe26b0fc444f5171b6f99
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2019-10-11 03:55:35 +0500

    start/end fixes of process

--- Update.bat
+++ Update.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 git clone https://github.com/Sheikhharis50/Stocker-Candela.git
+taskkill /IM Stocker.exe
 rm Stocker-Candela\update.bat
 echo ALL | move Stocker-Candela\*.* . 
 echo Y | rmdir /s Stocker-Candela
+start stocker.exe
 ::PAUSE
