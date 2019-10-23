6932b92a772785104ac6cdb34ade4e720ab8ddf5
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2019-10-23 23:33:17 +0500

    Add files via upload

--- Update.bat
+++ Update.bat
@@ -1,9 +1,11 @@
 @ECHO OFF
+attrib -h Update.bat
 taskkill /IM Stocker.exe
 ::git clone https://github.com/Sheikhharis50/Stocker-Candela.git
 git pull origin master
 ::rm Stocker-Candela\update.bat
 ::echo ALL | move Stocker-Candela\*.* . 
 ::echo Y | rmdir /s Stocker-Candela
+attrib +h Update.bat
 start stocker.exe
 ::PAUSE
